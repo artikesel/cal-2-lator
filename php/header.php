7570bf246b8342e2fdd3ecb06affc6de5d2ebe64
Vanix7 <gie1998@yandex.ru>
2020-01-23 01:21:03 +0300

    title

--- php/header.php
+++ php/header.php
@@ -13,7 +13,7 @@ echo <<<EOD
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>calculator</title>
+    <title>!!!calculator!!!!</title>
     <link rel="stylesheet" href="/Style/main.css">
     <link rel="stylesheet" href="/Style/matrix.css">
 </head>
